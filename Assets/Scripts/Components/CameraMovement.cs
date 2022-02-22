e20a8fd406232f854e220d7cb806c776a153287a
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-02-22 19:35:35 +0300

    SampleScene design

---
+++ Assets/Scripts/Components/CameraMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float camSpeed = 3;
+
+    void LateUpdate()
+    {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * camSpeed);
+    }
+}
